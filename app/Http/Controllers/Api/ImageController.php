8f474fb7884fed12637556cef364976341c988bf
AhmadTamem <145607000+AhmadTamem@users.noreply.github.com>
2025-04-30 17:20:34 +0300

    Add files via upload
    chat and image

---
+++ app/Http/Controllers/Api/ImageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\Service;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ImageController extends Controller
+{
+    public function uploadImage(Request $request, int $id)
+{
+    $request->validate([
+        'images' => 'required|array',
+        'images.*' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
+    ]);
+
+    $imageData = [];
+    $entity = null;
+    $filePath = '';
+
+    if ($request->get('type') == 'product') {
+        $entity = Product::find($id);
+        if (!$entity) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Product not found',
+            ], 404);
+        }
+        $filePath = 'images/product/';
+    } elseif ($request->get('type') == 'service') {
+        $entity = Service::find($id);
+        if (!$entity) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Service not found',
+            ], 404);
+        }
+        $filePath = 'images/service/';
+    }
+
+    foreach ($request->file('images') as $file) {
+        $fileName = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
+        $file->move(public_path($filePath), $fileName);
+
+        $imagePath = $filePath . $fileName;
+        $imageData[] = [
+            'product_id' => $request->get('type') == 'product' ? $entity->id : null,
+            'service_id' => $request->get('type') == 'service' ? $entity->id : null,
+            'user_id' => Auth::user()->id,
+            'image' => url($imagePath),
+        ];
+    }
+
+    Image::insert($imageData);
+
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Uploaded successfully',
+        'data' => $imageData,
+    ], 200);
+}
+}
